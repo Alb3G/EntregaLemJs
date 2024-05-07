c595a015f20a8f2ffe16db48beed0d6ac95c6ddc
Alberto <guzmanmorenoalberto1998@gmail.com>
2024-05-07 23:29:39 +0200

    funcion de mostrar contenedor planteada

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-function mostrarCaja() {
-	let cajaNota = document.createElement("div").setAttribute("class", "cajaNota");
+function showBox() {
+	const cajaNota = document.createElement("div");
+	cajaNota.classList.add("cajaNota");
+	cajaNota.textContent = "Hola soy un div";
+	const footer = document.querySelector('footer');
+	const body = document.querySelector('body');
+
+	body.insertBefore(cajaNota, footer);
 }
+
+function deleteBox() {}
+
